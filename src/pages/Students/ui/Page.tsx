72cf59b945f1c733e1861a82f6150fbb302824e6
Derman01 <lettenderman@gmail.com>
2023-03-28 01:41:13 +0500

    Доделал список студентов

--- src/pages/Students/ui/Page.tsx
+++ src/pages/Students/ui/Page.tsx
@@ -19,22 +19,24 @@ interface PageOptions extends ComponentOptions {
 
 export const Page: FC<PageOptions> = (options) => {
 	const {className} = options;
+	const [selectedGroup, setSelectedGroup]  = useState('');
 	const [filterStudents, setFilterStudents] = useState(FILTER_GROUP_INITIAL_STATE);
 
 	const groupLoadCallback = (items: object[]) => {
 		items.unshift(GROUP_ALL);
 	};
 
-	const changeFolderHandler = (item: {id: string}) => {
+	const changeFolderHandler = (item: {id: string, name: string}) => {
 		if (item.id === GROUP_ALL_ID) {
 			setFilterStudents({});
+			setSelectedGroup('')
 		} else {
 			setFilterStudents({
 				group: item.id
 			});
+			setSelectedGroup(item.name);
 		}
 	};
-
 
 	return (
 		<div className={classNames(['page__students', className])}>
@@ -58,11 +60,24 @@ export const Page: FC<PageOptions> = (options) => {
 			</div>
 			<div className="page__students_detail_container">
 				<div className="page__students_detail_container_header">
-					<div className="page__students_detail_container_title">
-						Группа
-						<span className={'page__students_detail_container_title-name'}>{}</span>
-					</div>
+					{
+						selectedGroup
+							?
+							<div className="page__students_detail_container_title">
+								Группа
+								<span className={'page__students_detail_container_title-name'}> {selectedGroup}</span>
+							</div>
+							:
+							<div className="page__students_detail_container_title">
+								Все группы
+							</div>
+					}
 					<Button icon={'plus'} iconSize={'m'}/>
+				</div>
+				<div className="page__students_detail_headers">
+					<div>ФИО</div>
+					<div>Категории прав</div>
+					<div>Оплата</div>
 				</div>
 				<View className={classNames('page__students_detail')}
 					  source={SOURCE_STUDENTS}
--- src/pages/Students/ui/templates/ItemTemplateStudent.tsx
+++ src/pages/Students/ui/templates/ItemTemplateStudent.tsx
@@ -2,20 +2,24 @@ import {FC} from 'react';
 import {StudentModel} from '../../models/StudentModel';
 
 export const ItemTemplateStudent: FC<StudentModel> = (student) => {
+	const [name, surname] = student.name.split(' ');
+
 	return (
 		<div className={'page__students_detail__item'}>
-			{/*<div className="photo"></div>*/}
+			<div className="page__students_detail__item_photo">{name[0]}{surname[0]}</div>
 			<div className="page__students_detail__item_info">
-                <span className={'page__students_detail__item_infoName'}>
-                    {student.ShortName}
+				<span className={'page__students_detail__item_infoName'}>
+                    {student.name}
                 </span>
 				<span className={'page__students_detail__item_infoGroup'}>
-					Группа {student.group_name}
+					{student.group_name}
                 </span>
 			</div>
-			<div className={'page__students_detail__item_payment'}
-				  title={'Задолженность ' + student.payment_needed + ' руб.'}>
-				<span>{student.payment_needed} руб.</span>
+			<div className={'page__students_detail__item_category'}>
+				<span>B</span>
+			</div>
+			<div className={'page__students_detail__item_category'}>
+				<span>{student.payment_needed} %</span>
 			</div>
 		</div>
 	);
